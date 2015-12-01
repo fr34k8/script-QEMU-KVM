c659eb4470d37de98d18cf67061412ce7bf2a738
Juan Miguel Navarro Martínez <juanmi.3000@gmail.com>
2015-12-01 18:02:07 +0100

    Creation of configuration files and more checks.

--- kvm_vm_backup.sh
+++ kvm_vm_backup.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
-CURRENT_PATH=$(pwd)
+
+# Create configuration file.
+if [ -f kvm_vm_backup.conf ]; then
+	cat<<EOF
+POOLDIR=""
+BACKUPDIR="\$POOLDIR/Backups"
+EOF
+fi
+
+# Load variables
+CONFFILE="./kvm_vm_backup.conf"
+source kvm_vm_backup.conf
 IMG_FILE="$1"
 NOM_VM="$(basename ${IMG_FILE} .img)"
 
+increment_renaming(){
+	TIMESTAMP="$(date +%Y%m%d_%H%M%S)"
+	NOM_INC="${NOM_VM}_${TIMESTAMP}"
+
+	mv ${NOM_VM}.img ${NOM_INC}.inc.img
+}
+
 apagar(){
-	## Check that Virtual Machine is turned off
 	COUNTER=0
+
+	## Turn off machine
+	virsh shutdown ${NOM_VM}
+
+	## Check that Virtual Machine is turned off
 	while [[ $(virsh domstate ${NOM_VM} | egrep 'apagado' | wc -l) -eq 0 ]]; do
 		echo "Waiting for VM to shut down..."
 		sleep 10
@@ -14,7 +36,7 @@ apagar(){
 		## Exit script if it doesn't turn off.
 		if [ $COUNTER -ge 5 ]; then
 			echo "VM can't be stopped or is waiting to long, aborting script."
-			exit
+			exit 1
 		fi
 	done
 
@@ -24,16 +46,13 @@ apagar(){
 incremental(){
 	echo "Creating new snapshot."
 	## Rename snapshot.
-	TIMESTAMP="$(date +%Y%m%d_%H%M%S)"
-	NOM_INC="${NOM_VM}_${TIMESTAMP}"
-
-	mv ${NOM_VM}.img ${NOM_INC}.img
+	increment_renaming
 
 	## Back-up snapsot using hard link.
-	ln ${NOM_INC}.img Backups/${NOM_INC}.img
+	ln ${NOM_INC}.img Backups/${NOM_INC}.inc.img
 
 	## Create a new snapshot using previous one as backing file.
-	qemu-img create -b ${NOM_INC}.img -f qcow2 ${NOM_VM}.img
+	qemu-img create -b ${NOM_INC}.img -f qcow2 ${NOM_VM}.inc.img
 
 	## For non-privileged users, add read and write permisions to others.
 	chmod o=rw ${NOM_VM}.img
@@ -43,19 +62,16 @@ incremental(){
 
 rebase(){
 	echo "Rebasing the snapshots."
-	# Renane snapshot.
-	TIMESTAMP="$(date +%Y%m%d_%H%M%S)"
-	NOM_INC="${NOM_VM}_${TIMESTAMP}"
-
-	mv ${NOM_VM}.img ${NOM_INC}.img
+	# Rename snapshot.
+	increment_renaming
 
 	# Rebasing the last snapshot and backing it up as a hard link.
-	qemu-img convert -O qcow2 ${NOM_INC}.img ${NOM_INC}.img.rebase
-	ln ${NOM_INC}.img.rebase Backups/${NOM_INC}.img.rebase
+	qemu-img convert -O qcow2 ${NOM_INC}.img ${NOM_INC}.rebase.img
+	ln ${NOM_INC}.img.rebase Backups/${NOM_INC}.rebase.img
 
 	echo "The new base (${NOM_INC}.img.rebase) has been created."
 
-	# Deleting old unnecesary snapshots. 
+	# Deleting old unnecesary snapshots.
 	REBASE_FILE="$(ls ${NOM_VM}* | grep rebase)"
 	if [ -n "${REBASE_FILE}" ]; then
 		REMOVE_LIST=$(ls -l ${NOM_VM}_*.img | grep -v "rebase")
@@ -65,21 +81,31 @@ rebase(){
 		NEW_BASE="$(basename ${REBASE_FILE} .rebase)"
 		mv "${REBASE_FILE}" "${NEW_BASE}"
 	fi
-	
+
 	# Creating a new snapshot with the new base as backing file.
 	qemu-img create -b ${NEW_BASE} -f qcow2 ${NOM_VM}.img
 	echo -e "Old snapshots have been deleted.\nA new snapshot (${NOM_VM}.img) have been created and ready to be used."
 }
 
+rsync_backup(){
+	rsync -aw
+}
 # Creating "Backups" folder if it does not exit.
-mkdir -p Backups
+if [ -z $POOLDIR ]; then
+	read -p "Please, input your VM pool folder path: " POOLDIR
+	POOLDIR=`readlink -m $POOLDIR`
+	sed -i "s/POOLDIR=\".*\"/POOLDIR=\"$POOLDIR\"/g" $CONFFILE
+fi
+
+if [ ! -d $BACKUPDIR ]; then
+	mkdir -p $BACKUPDIR
+fi
 
 COUNTER=$(qemu-img info --backing-chain ${NOM_VM}.img | grep "backing file" | wc -l )
 MAX_COUNTER=3
 
 if [[ $(virsh domstate ${NOM_VM} | egrep 'apagado' | wc -l) -eq 0 ]]; then
 	echo "Turning off the VM."
-	virsh shutdown ${NOM_VM}
 	apagar
 fi
 
@@ -90,4 +116,3 @@ else
 fi
 echo "Starting up the VM."
 virsh start ${NOM_VM}
-
